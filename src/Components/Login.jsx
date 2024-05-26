8257a07e017f9908f5adec81c77b6bdab54ead2a
Your Name <you@example.com>
2024-05-26 17:44:58 -0300

    first commit

---
+++ src/Components/Login.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FaUser, FaLock } from 'react-icons/fa';
+
+import { useState } from "react";
+
+import "./Login.scss";
+
+
+const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        alert("Enviando os dados: " + username + " e " + password);
+
+    };
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h1>Acesse o sistema</h1>
+                <div className='input-field'>
+                    <input type="email" placeholder='E-mail' required
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <FaUser className='icon' />
+                </div>
+                <div className='input-field'>
+                    <input type="password" placeholder='Senha' required
+                        onChange={(e) => setPassword(e.target.value)} />
+                    <FaLock className='icon' />
+                </div>
+
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox" />
+                        Lembre de mim
+                    </label>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+                <button>Entrar</button>
+
+                <div className="signup-link">
+                    <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
